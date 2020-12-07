96e1427d6514552e57af7106408c47e10218e58c
astroluj <astroluj@neosecu.com>
2020-12-07 09:29:25 +0900

    listener 파일 이름 변경 및 jitpack연결

---
+++ app/src/main/java/com/android/serial/SerialInputOutputListener.java
@@ -0,0 +1,28 @@
+/* Copyright 2011-2013 Google Inc.
+ * Copyright 2013 mike wakerly <opensource@hoho.com>
+ *
+ * Project home page: https://github.com/mik3y/usb-serial-for-android
+ */
+
+package com.android.serial;
+
+/**
+ *
+ * @author mike wakerly (opensource@hoho.com)
+ */
+public interface SerialInputOutputListener {
+    /**
+     * Ready when incoming is connect.
+     */
+    void onReadyRun();
+
+    /**
+     * Called when new incoming data is available.
+     */
+    void onNewData(byte[] data);
+
+    /**
+     * Called aborts due to an error.
+     */
+    void onRunError(Exception e);
+}
