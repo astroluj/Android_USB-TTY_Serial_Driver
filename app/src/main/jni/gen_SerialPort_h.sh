3cece84fda6492613ac40a265148eca9aaf29d6e
astroluj <astroluj@neosecu.com>
2020-12-07 10:49:27 +0900

    패키지 이름 변경

--- app/src/main/jni/gen_SerialPort_h.sh
+++ app/src/main/jni/gen_SerialPort_h.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-javah -o SerialPort.h -jni -classpath ../src com.android.serial.ttyserial.SerialPort
+javah -o SerialPort.h -jni -classpath ../src com.astroluj.serial.ttyserial.SerialPort
 
